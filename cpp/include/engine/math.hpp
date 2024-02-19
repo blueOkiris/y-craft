a3152049464ce8deaa6facb60306e22511b293fd
Dylan Turner <dylan.turner@tutanota.com>
2024-02-18 19:03:54 -0600

    cpp: deleted (old x-craft source)
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- cpp/include/engine/math.hpp
+++
@@ -1,8 +0,0 @@
-// Common math functions used in game programming
-
-#pragma once
-
-namespace math {
-    double lerp(const double val, const double approach, const double weight);
-}
-
--- cpp/src/engine/math.cpp
+++
@@ -1,8 +0,0 @@
-// Implement common math functions
-
-#include <engine/math.hpp>
-
-double math::lerp(const double val, const double approach, const double weight) {
-    return val * (1.0 - weight) + (approach * weight);
-}
-
