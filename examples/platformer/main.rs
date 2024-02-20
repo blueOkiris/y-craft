c5ac378f81548cc59e81aa7321f035d17f374fa1
Dylan Turner <dylan.turner@tutanota.com>
2024-02-19 22:53:48 -0600

    platformer: added
    Will make a demo of platformer physics
    
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

---
+++ examples/platformer/main.rs
@@ -0,0 +1,31 @@
+/*use std::collections::HashMap;
+use sdl2::pixels::Color;
+use ycraft::{
+    app::run,
+    obj::ControlObjectBehavior
+};
+
+const DEF_WIN_WIDTH: u32 = 640;
+const DEF_WIN_HEIGHT: u32 = 360;
+const FPS: f64 = 60.0;
+const BG_COLOR: Color = Color::RGB(0x60, 0x60, 0x80);
+*/
+fn main() -> Result<(), String> {
+    /*let snds = [
+    ];
+    let imgs = [
+    ];
+    let fonts = [
+    ];
+
+    let rooms = HashMap::from([
+    ]);
+    let ctl_objs: Vec<Box<dyn ControlObjectBehavior<_, _, _, _, _, _>>> = vec![
+    ];
+
+    run(
+        "Y-Craft", DEF_WIN_WIDTH, DEF_WIN_HEIGHT, FPS, &BG_COLOR,
+        Rm::Title, &rooms, &ctl_objs, &snds, &imgs, &fonts
+    )*/Ok(())
+}
+
